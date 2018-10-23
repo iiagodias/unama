0d239be30732a3a3ae93979e5c2487ddf1bf73b0
26002528 <26002528@UNA009-L102-002.serlab.intra>
2018-10-22 21:33:17 -0300

    Commit Inicial

---
+++ Projeto1/src/br/com/exemplo/OlaMundo.java
@@ -0,0 +1,44 @@
+package br.com.exemplo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
+
+
+@WebServlet("/exemplo")
+public class OlaMundo extends HttpServlet  {
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name="jdbc/web-aluno")
+	private DataSource dataSource;	
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		Connection con = null;
+		try {
+			con = dataSource.getConnection();
+			PrintWriter out = response.getWriter();
+			out.println("<html>");
+			out.println("<body>");
+			out.println("Conexão efetuada com sucesso.");
+			out.println("</body>");
+			out.println("</html>");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
